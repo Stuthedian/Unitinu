775b8d6b68153a707ff77c5958ac68fc42357a8b
Stuthedian <50074364+Stuthedian@users.noreply.github.com>
2019-10-27 15:49:08 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SaS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отразитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.копироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.всёИзображениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выделениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вставитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.SelectionButton1 = new System.Windows.Forms.ToolStripButton();
+            this.MirrorButton = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(639, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.отразитьToolStripMenuItem,
+            this.копироватьToolStripMenuItem,
+            this.вставитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // отразитьToolStripMenuItem
+            // 
+            this.отразитьToolStripMenuItem.Name = "отразитьToolStripMenuItem";
+            this.отразитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.отразитьToolStripMenuItem.Text = "Отразить";
+            this.отразитьToolStripMenuItem.Click += new System.EventHandler(this.отразитьToolStripMenuItem_Click);
+            // 
+            // копироватьToolStripMenuItem
+            // 
+            this.копироватьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.всёИзображениеToolStripMenuItem,
+            this.выделениеToolStripMenuItem});
+            this.копироватьToolStripMenuItem.Name = "копироватьToolStripMenuItem";
+            this.копироватьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.копироватьToolStripMenuItem.Text = "Копировать";
+            // 
+            // всёИзображениеToolStripMenuItem
+            // 
+            this.всёИзображениеToolStripMenuItem.Name = "всёИзображениеToolStripMenuItem";
+            this.всёИзображениеToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.всёИзображениеToolStripMenuItem.Text = "Всё изображение";
+            this.всёИзображениеToolStripMenuItem.Click += new System.EventHandler(this.всёИзображениеToolStripMenuItem_Click);
+            // 
+            // выделениеToolStripMenuItem
+            // 
+            this.выделениеToolStripMenuItem.Name = "выделениеToolStripMenuItem";
+            this.выделениеToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.выделениеToolStripMenuItem.Text = "Выделение";
+            this.выделениеToolStripMenuItem.Click += new System.EventHandler(this.выделениеToolStripMenuItem_Click);
+            // 
+            // вставитьToolStripMenuItem
+            // 
+            this.вставитьToolStripMenuItem.Name = "вставитьToolStripMenuItem";
+            this.вставитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.вставитьToolStripMenuItem.Text = "Вставить";
+            this.вставитьToolStripMenuItem.Click += new System.EventHandler(this.вставитьToolStripMenuItem_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.trackBar1.Location = new System.Drawing.Point(0, 24);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 401);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickFrequency = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(45, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(594, 401);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(592, 399);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.SelectionButton1,
+            this.MirrorButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 425);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(639, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.toolStripComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Standard;
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.ToolTipText = "Масштаб изображения";
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // SelectionButton1
+            // 
+            this.SelectionButton1.CheckOnClick = true;
+            this.SelectionButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.SelectionButton1.Image = ((System.Drawing.Image)(resources.GetObject("SelectionButton1.Image")));
+            this.SelectionButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SelectionButton1.Name = "SelectionButton1";
+            this.SelectionButton1.Size = new System.Drawing.Size(23, 22);
+            this.SelectionButton1.Text = "toolStripButton1";
+            this.SelectionButton1.ToolTipText = "Выделение";
+            this.SelectionButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // MirrorButton
+            // 
+            this.MirrorButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.MirrorButton.Image = ((System.Drawing.Image)(resources.GetObject("MirrorButton.Image")));
+            this.MirrorButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.MirrorButton.Name = "MirrorButton";
+            this.MirrorButton.Size = new System.Drawing.Size(23, 22);
+            this.MirrorButton.Text = "toolStripButton2";
+            this.MirrorButton.ToolTipText = "Отражение";
+            this.MirrorButton.Click += new System.EventHandler(this.отразитьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(639, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Смемсь";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem отразитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem копироватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem всёИзображениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton SelectionButton1;
+        private System.Windows.Forms.ToolStripMenuItem выделениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вставитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton MirrorButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace SaS
+{
+    public partial class Form1 : Form
+    {
+        Image image, originalimage, tempimage;
+        Bitmap tempbitmap;
+        bool mirrored = false;
+        Point startpoint, endpoint;
+        Pen selectionpen = new Pen(Color.Gray)
+        {
+            DashStyle = DashStyle.Dash,
+            Width = 4
+        };
+        Rectangle rectangle;
+        Graphics graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            копироватьToolStripMenuItem.Enabled = false;
+            выделениеToolStripMenuItem.Enabled = false;
+            отразитьToolStripMenuItem.Enabled = false;
+            toolStripComboBox1.ComboBox.ValueMember = "Value";
+            toolStripComboBox1.ComboBox.DisplayMember = "Text";
+            foreach (ToolStripItem item in toolStrip1.Items)
+            {
+                item.Enabled = false;
+            }
+            trackBar1.Enabled = false;
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName == "")
+                return;
+            try
+            {
+                image = Image.FromFile(openFileDialog1.FileName);
+                originalimage = (Image)image.Clone();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\nНе удалось открыть файл", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                openFileDialog1.FileName = "";
+                return;
+            }
+            process_image();
+        }
+        
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if(trackBar1.Value != 0)
+            {
+                Rectangle rectangle = new Rectangle(-1, 0, trackBar1.Value, image.Height);
+                Bitmap bitmap1 = new Bitmap(image);
+                if (mirrored)
+                    rectangle.X = image.Width - trackBar1.Value;
+                else
+                    rectangle.X = 0;
+                bitmap1 = bitmap1.Clone(rectangle, image.PixelFormat);
+                Bitmap emptybit = new Bitmap(bitmap1.Width * 2, image.Height);
+                if (mirrored)
+                    bitmap1.RotateFlip(RotateFlipType.Rotate180FlipY);
+                Graphics.FromImage(emptybit).DrawImage(bitmap1, 0, 0);
+                bitmap1.RotateFlip(RotateFlipType.Rotate180FlipY);
+                Graphics.FromImage(emptybit).DrawImage(bitmap1, bitmap1.Width, 0);
+                pictureBox1.Image = emptybit;
+                tempimage = pictureBox1.Image;
+                GC.Collect();
+            }
+        }
+
+        private void отразитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mirrored = !mirrored;
+            trackBar1_Scroll(sender, e);
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBoxItem a = (ComboBoxItem)toolStripComboBox1.Items[toolStripComboBox1.SelectedIndex];
+            resize_image(a.Value);
+        }
+
+        private void resize_image(double multiplier)
+        {
+            Rectangle destRect = new Rectangle(0, 0, Convert.ToInt32(originalimage.Width * multiplier),
+                Convert.ToInt32(originalimage.Height * multiplier));
+            Bitmap destImage = new Bitmap(destRect.Width, destRect.Height);
+            destImage.SetResolution(originalimage.HorizontalResolution, originalimage.VerticalResolution);
+            using (Graphics graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (ImageAttributes wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(originalimage, destRect, 0, 0, originalimage.Width, originalimage.Height, 
+                        GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            image = destImage;
+            trackBar1.SetRange(0, image.Width);
+            trackBar1.Value = image.Width / 2;
+            trackBar1_Scroll(new object(), new EventArgs());
+        }
+
+        private void всёИзображениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetImage(pictureBox1.Image);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            startpoint.X = e.X;
+            startpoint.Y = e.Y;
+            rectangle = new Rectangle(startpoint.X, startpoint.Y, 0, 0);
+            tempbitmap = new Bitmap(pictureBox1.Image.Width, pictureBox1.Image.Height);
+            graphics = Graphics.FromImage(tempbitmap);
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            endpoint.X = e.X;
+            endpoint.Y = e.Y;
+            rectangle.Width = Math.Abs(endpoint.X - rectangle.X);
+            rectangle.Height = Math.Abs(endpoint.Y - rectangle.Y);
+            graphics.DrawImage(tempimage, 0, 0);
+            graphics.DrawRectangle(selectionpen, rectangle);
+            pictureBox1.Image = tempbitmap;
+        }
+
+        private void выделениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            graphics.DrawImage(tempimage, 0, 0);
+            graphics.DrawRectangle(new Pen(Color.Transparent), rectangle);
+            Bitmap bitmap = ((Bitmap)pictureBox1.Image).Clone(rectangle, pictureBox1.Image.PixelFormat);
+            Clipboard.SetImage(bitmap);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pictureBox1.MouseMove -= pictureBox1_MouseMove;
+        }
+
+        private void вставитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetImage() == null)
+            {
+                MessageBox.Show("Буфер обмена пуст, либо его содержимое не является изображением", "Вставка невозможна",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                image = Clipboard.GetImage();
+                originalimage = (Image)image.Clone();
+                process_image();
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if(SelectionButton1.Checked)
+            {
+                pictureBox1.Cursor = Cursors.Cross; 
+                pictureBox1.MouseDown += pictureBox1_MouseDown;
+                выделениеToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                выделениеToolStripMenuItem.Enabled = false;
+                pictureBox1.MouseDown -= pictureBox1_MouseDown;
+                pictureBox1.Image = tempimage;
+                pictureBox1.Cursor = Cursors.Default;
+            }
+        }
+
+        private void process_image()
+        {
+            toolStripComboBox1.SelectedIndexChanged -= toolStripComboBox1_SelectedIndexChanged;
+            toolStripComboBox1.ComboBox.DataSource = new ComboBoxItem[] {
+                new ComboBoxItem{ Value = 0.5, Text = "50%" },
+                new ComboBoxItem{ Value = 0.75, Text = "75%" },
+                new ComboBoxItem{ Value = 1.0, Text = "100%" }
+            };
+            toolStripComboBox1.SelectedIndexChanged += toolStripComboBox1_SelectedIndexChanged;
+            toolStripComboBox1.ComboBox.SelectedValue = 1.0;
+            if (this.Width >= Screen.PrimaryScreen.WorkingArea.Width
+                || this.Height >= Screen.PrimaryScreen.WorkingArea.Height)
+            {
+                double mulW = Screen.PrimaryScreen.WorkingArea.Width / (originalimage.Width * 1.0);
+                double mulH = Screen.PrimaryScreen.WorkingArea.Height / (originalimage.Height * 1.0);
+                double result = Math.Round(mulW > mulH && mulW < 1.0 ? mulW * 0.7 : mulH * 0.7, 2);
+
+                resize_image(result);
+                var a = ((ComboBoxItem[])toolStripComboBox1.ComboBox.DataSource).ToList();
+                a.Add(new ComboBoxItem()
+                {
+                    Value = result,
+                    Text = Convert.ToInt32(result * 100).ToString() + "%"
+                });
+                toolStripComboBox1.SelectedIndexChanged -= toolStripComboBox1_SelectedIndexChanged;
+                a = a.OrderBy(m => m.Value).ToList();
+                var i = a.FindIndex(m => m.Value == result);
+                toolStripComboBox1.ComboBox.DataSource = a.Take(i + 1).ToList();
+                toolStripComboBox1.ComboBox.SelectedValue = result;
+                toolStripComboBox1.SelectedIndexChanged += toolStripComboBox1_SelectedIndexChanged;
+            }
+            else
+            {
+                trackBar1.SetRange(0, image.Width);
+                trackBar1.Value = image.Width / 2;
+                trackBar1_Scroll(new object(), new EventArgs());
+            }
+            копироватьToolStripMenuItem.Enabled = true;
+            отразитьToolStripMenuItem.Enabled = true;
+            foreach (ToolStripItem item in toolStrip1.Items)
+            {
+                item.Enabled = true;
+            }
+            trackBar1.Enabled = true;
+            Location = new Point(Screen.PrimaryScreen.WorkingArea.Width/2 - image.Width/2 , 
+                Screen.PrimaryScreen.WorkingArea.Height/2 - image.Height/2);
+        }
+
+        private class ComboBoxItem
+        {
+            public double Value { get; set; }
+            public string Text { get; set; }
+        }
+    }
+}
